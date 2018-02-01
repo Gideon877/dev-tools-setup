f447b4cde70f3c619358889d10a3340c9e54a685
Michael Duminy <mike@getwala.com>
2018-02-01 15:47:40 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -44,7 +44,7 @@ brew cask install --appdir="/Applications" firefox
 brew cask install --appdir="/Applications" skype
 brew cask install --appdir="/Applications" slack
 
-if test -ne .bash_profile then touch .bash_profile
+if test -ne .bash_profile then touch .bash_profile fi
 source .bash_profile
 
 curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
