cbc6dcdbc3dbdeaf72fd58bb11137330d206dc7e
Thabang Gideon <gideon877@live.com>
2024-11-04 11:36:53 +0200

    Update brew.sh

--- brew.sh
+++ brew.sh
@@ -8,59 +8,62 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# Check for Homebrew,
-# Install if we don't have it
-if test ! $(which brew); then
-  echo "Installing homebrew..."
-  ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+# Check for Homebrew, install if we don't have it
+if ! command -v brew &> /dev/null; then
+  echo "Installing Homebrew..."
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 fi
 
 # Make sure we’re using the latest Homebrew.
 brew update
 
 # Upgrade any already-installed formulae.
-brew upgrade --all
+brew upgrade
 
-brew install git
-brew install git-lfs
-brew install git-flow
-brew install git-secrets
+# Install essential tools
+brew install git git-lfs git-flow git-secrets
 
-# Install Cask
-brew install caskroom/cask/brew-cask
-brew tap caskroom/versions
+# Install Cask (now part of Homebrew)
+brew install --cask java8 android-studio google-chrome firefox skype slack visual-studio-code
 
-# Core casks
-brew cask install --appdir="~/Applications" caskroom/versions/java8
-brew cask install --appdir="~/Applications" android-studio
-brew cask install android-sdk
+# Install Docker and related tools
+brew install docker docker-compose
 
-# Development tool casks
-brew cask install --appdir="/Applications" visual-studio-code
-brew install docker docker-compose docker-machine xhyve docker-machine-driver-xhyve
+# Install Node.js and npm (using NVM for version management)
+if ! command -v nvm &> /dev/null; then
+  echo "Installing NVM..."
+  curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.5/install.sh | bash
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
+fi
+nvm install node  # Install the latest Node.js
+npm install -g npm@latest  # Update npm to the latest version
 
-# Misc casks
-brew cask install --appdir="/Applications" google-chrome
-brew cask install --appdir="/Applications" firefox
-brew cask install --appdir="/Applications" skype
-brew cask install --appdir="/Applications" slack
+# Install global npm packages for React and React Native
+npm install -g create-react-app react-native-cli
 
-[ ! -e ~/.bash_profile ] && touch ~/.bash_profile
-source ~/.bash_profile
+# Install Yarn
+brew install yarn
 
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.8/install.sh | bash
-brew install yarn --without-node
-echo "------------------------------"
-echo "Setting up pip."
+# Install Dart and Flutter
+brew tap dart-lang/dart
+brew install dart
+brew install --cask flutter
+
+# Install databases
+brew install mongodb-community@6.0  # MongoDB
+brew install postgresql  # PostgreSQL
+brew install redis  # Redis
+
+# Install AWS CLI
 brew install python3
+pip3 install --upgrade pip
 pip3 install awscli
-nvm install 6.10.2
 
+# Install global npm packages for linting and serverless
+npm install -g eslint serverless
+
+# Cleanup outdated versions
 brew cleanup
-npm install -g eslint
-npm install -g react-native-cli
-npm install -g serverless
 
-
-# Remove outdated versions from the cellar.
-brew cleanup
+echo "Development environment setup complete!"
